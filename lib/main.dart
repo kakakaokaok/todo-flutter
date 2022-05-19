4f8f6ab0a9ff4d5afef4ab0907011ebe62eab6ff
thebjko <thebjko@gmail.com>
2022-05-19 15:42:07 +0900

    45. Injecting Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/models/todo_model.dart';
 
 import 'pages/todos_page.dart';
+import 'providers/providers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +14,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODOS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (context) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (context) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (context) => TodoList(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (
+            BuildContext context,
+            TodoList todoList,
+            ActiveTodoCount? activeTodoCount,
+          ) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(),
+          update: (
+            BuildContext context,
+            TodoFilter todoFilter,
+            TodoSearch todoSearch,
+            TodoList todoList,
+            FilteredTodos? filteredTodos,
+          ) =>
+              filteredTodos!..update(todoFilter, todoSearch, todoList),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const TodosPage(),
       ),
-      home: const TodosPage(),
     );
   }
 }
